885b68db2d8a40ce35d2efc80aa8be667e26ccc1
Valentine <valentinemaillard1@gmail.com>
2020-01-05 13:03:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+    let thankYouCard = []
+    for (let i = 0; i < names.length; i++){
+
+        thankYouCard.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouCard
+}
+
+function countDown(num){
+
+    while( num > 0){
+        console.log(num)
+        num -= 1
+    }
+    console.log(num)
+}
