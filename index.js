9d2ccd2ac89e63e65f4562d6004e061ce0cea7d8
asuthamm <asuthamm@gmail.com>
2020-01-05 12:38:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(arr, str) {
+    const arrOut = []; 
+    for (let i=0; i < arr.length; i++) {
+        arrOut.push(`Thank you, ${arr[i]}, for the wonderful ${str} gift!`);
+    }
+    return arrOut;
+}
+writeCards( [ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' );
+
+function countDown(num) {
+    for(let i=num; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+
