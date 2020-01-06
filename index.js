f5389978bd4b491e0f06ab7dedc5fc6ae5e4e6ab
BenMiriello <benmiriello@gmail.com>
2020-01-05 20:47:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(names, occasion) {
+  let i = 0;
+  let cards = [];
+  while (i < names.length) {
+    cards[i] = (`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    i ++;
+  }
+  // console.log(cards)
+  return cards;
+}
+
+function countDown(start) {
+  while (start >= 0) {
+    console.log(start);
+    start = start - 1;
+  }
+}
